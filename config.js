cfb1a99c9101cb5d0edf0703ed0a1e28d9745fa0
iqballlshahh <164973243+iqballlshahh@users.noreply.github.com>
2026-05-22 14:11:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
   GOOGLE_CLIENT_ID:'447120741421-os8qgn9lnjsdn0m2tti2i47a7u0rp48i.apps.googleusercontent.com',
   AWS_REGION:'ap-southeast-1',
   COGNITO_IDENTITY_POOL_ID:'ap-southeast-1:7c43827d-f34c-4613-83d1-963b19338ec2',
-  IOT_ENDPOINT:'alal6zi3dqzif-ats.iot.ap-southeast-1.amazonaws.com',
+  IOT_ENDPOINT:'alal6zi3dqzif-ats.iot.ap-southeast-1.amazonaws.com'
 };
